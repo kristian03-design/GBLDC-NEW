3cb69d993c155305118d3fb653316c0973ae4547
Kristian Hernandez <hkristianlloyd2@gmail.com>
2025-04-11 12:19:50 +0800

    `Removed mobile menu and dropdown functionality`

--- src/javascript/dropdown.js
+++ src/javascript/dropdown.js
@@ -9,17 +9,3 @@ function toggleDropdownProducts(event) {
     dropdown.classList.toggle('hidden');
 }
 
-function toggleMobileMenu() {
-    const menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('hidden');
-      }
-      function toggleDropdownProducts(event) {
-    event.preventDefault();
-    const dropdown = document.getElementById('dropdown-menu-products-mobile');
-    dropdown.classList.toggle('hidden');
-      }
-      function toggleDropdown(event) {
-    event.preventDefault();
-    const dropdown = document.getElementById('dropdown-menu-mobile');
-    dropdown.classList.toggle('hidden');
-      }
