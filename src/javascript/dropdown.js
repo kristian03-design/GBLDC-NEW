732a08618802ae670313759a7ffefa6fe60e20b5
Kristian Hernandez <hkristianlloyd2@gmail.com>
2025-04-11 12:02:10 +0800

    `Refactor HTML and CSS for responsive design and mobile menu functionality`

--- src/javascript/dropdown.js
+++ src/javascript/dropdown.js
@@ -8,3 +8,18 @@ function toggleDropdownProducts(event) {
     const dropdown = document.getElementById('dropdown-menu-products');
     dropdown.classList.toggle('hidden');
 }
+
+function toggleMobileMenu() {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+      }
+      function toggleDropdownProducts(event) {
+    event.preventDefault();
+    const dropdown = document.getElementById('dropdown-menu-products-mobile');
+    dropdown.classList.toggle('hidden');
+      }
+      function toggleDropdown(event) {
+    event.preventDefault();
+    const dropdown = document.getElementById('dropdown-menu-mobile');
+    dropdown.classList.toggle('hidden');
+      }
