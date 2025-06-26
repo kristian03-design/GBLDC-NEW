2db87088b059d3a2a6985eadd97717038fe49f57
Kristian Hernandez <hkristianlloyd2@gmail.com>
2025-06-26 14:59:36 +0800

    Updates image paths and styling for better responsiveness
    Modifies image sources and dimensions across various sections to enhance visual consistency and responsiveness.
    
    Adjusts carousel settings for improved performance and user experience.
    
    Removes unused CSS variables to streamline the stylesheet.
    
    Adds new images to the landing page for more engaging content.
    
    Updates images and styles for improved responsiveness
    
    Modifies image sources and dimensions across various sections to enhance visual consistency and responsiveness.
    
    Adjusts carousel settings for better performance and user experience.
    
    Removes unused CSS variables to streamline the stylesheet.
    
    Adds new images to the landing page for more engaging content.

--- src/javascript/carousel.js
+++ src/javascript/carousel.js
@@ -1,36 +1,37 @@
- $(document).ready(function(){
-        $('.carousel-slider').slick({
-          centerMode: false,
-          centerPadding: '0px',
-          dots: false,
-          arrows: false,
-          infinite: true,
-          speed: 1000,
-          fade: false,
-          cssEase: 'linear',
-          slidesToShow: 3,
-          slidesToScroll: 1,
-         
-          autoplay: true,
-          autoplaySpeed: 3500,
-          adaptiveHeight: true,
-          responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 2,
-            centerMode: false,
-            centerPadding: '0px'
-          }
-        },
-        {
-          breakpoint: 640,
-          settings: {
-            slidesToShow: 1,
-            centerMode: true,
-            centerPadding: '0px'
-          }
-        }
-          ]
-        });
-      });
+$(document).ready(function(){
+    $('.carousel-slider').slick({
+        centerMode: false,
+        centerPadding: '0px',
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        fade: false,
+        cssEase: 'linear',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3500,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: false,
+                    centerPadding: '0px'
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                    centerPadding: '0px'
+                }
+            }
+        ]
+    });
+});
